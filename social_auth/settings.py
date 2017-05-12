c04fec7b9613ea5ee7d6da381c5fcf0a2a35d875
jdavidagudelo <jdaaa2009@gmail.com>
2017-05-12 16:44:51 -0500

    Fixing issues.

---
+++ social_auth/settings.py
@@ -0,0 +1,7 @@
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'social_auth',
+)
+SECRET_KEY = 'h$qx-21=w81h6z9tli)#3p+poa#aejkyy)v^0a=1!0=)zw@a-i'
